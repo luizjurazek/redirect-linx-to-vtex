0a79fb41b4e0ddaad4d41a0b24fc7449757822e1
Luiz Jurazek <luizjurazek@gmail.com>
2023-09-11 22:17:50 -0300

    Func para comparar arrays e escrevendo os dados em csv

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const fs = require("fs")
-const csvtojson = require("csvtojson")
+const fs = require("fs");
+const csvtojson = require("csvtojson");
+const {
+    createObjectCsvWriter
+} = require('csv-writer');
 
 // Path do csv 
-const produtosVTEXCSV = './csv/produtos.csv'
-const produtosLINXCSV = './csv/internal_all-venture.csv'
+const produtosVTEXCSV = './csv/produtos.csv';
+const produtosLINXCSV = './csv/produtos-venture.csv';
+const caminhoArquivo = './csv/dados.csv';
+
 
 // Variável global para armazenar os produtos
-let arrayProdutosVTEX = []
-let arrayProdutosLINX = []
+let produtosIguais = [];
+
 
 // Converter CSV em JSON
 async function converterCSVparaJSON(path) {
@@ -25,19 +30,19 @@ async function converterCSVparaJSON(path) {
 async function obterProdutosVTEX(path) {
     try {
         const JSONProdutos = await converterCSVparaJSON(path)
-        arrayProdutosVTEX = []; // Limpa o array antes de preenchê-lo novamente
+        let arrayProdutosVTEX = []
 
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < JSONProdutos.length; i++) {
             const produto = JSONProdutos[i];
             arrayProdutosVTEX.push({
-                Nome: produto['_NomeProduto (Obrigatório)'],
-                Categoria: produto['_IDCategoria'],
-                SKU: produto['_IDSKU (Não alterável)'],
-                link: produto['_LinkTexto (Não alterável)']
+                nome: produto['_NomeProduto (Obrigatório)'],
+                // Categoria: produto['_IDCategoria'],
+                link: produto['_LinkTexto (Não alterável)'],
+                skuProduto: parseInt(produto['_IDSKU (Não alterável)'])
             })
         }
         return arrayProdutosVTEX
-    } catch(erro){
+    } catch (erro) {
         console.error(erro)
     }
 }
@@ -45,39 +50,92 @@ async function obterProdutosVTEX(path) {
 // Obter apenas os campos necessários dos produtos LINX que estão no CSV 
 async function obterProdutosLINX(path) {
     try {
-        const JSONProdutos = await converterCSVparaJSON(path)
-        arrayProdutosLINX = []; // Limpa o array antes de preenchê-lo novamente
 
-        for (let i = 0; i < 3; i++) {
+        const JSONProdutos = await converterCSVparaJSON(path)
+        const regexForSkuInTheURL = /-(\d+)-/ // Regex para procurar a ocorrencia de sku na url do produto 
+        let arrayProdutosLINX = []
+        console.log()
+        for (let i = 0; i < JSONProdutos.length; i++) {
             const produto = JSONProdutos[i];
 
             // Expressão regular para extrair o sku
-            let skuMatch = produto['Address'].match(/-(\d+)-/)
+            let skuMatch = produto['Address'].match(regexForSkuInTheURL)
 
-            // Adiciona o dados do produtos em um array de objetos
+            // Adiciona os dados do produto em um array de objetos
             arrayProdutosLINX.push({
-                Nome: produto['Title 1'],
+                nome: produto['Title 1'],
                 link: produto['Address'],
                 skuProduto: parseInt(skuMatch ? skuMatch[1] : null), // Capturar o SKU se Match for encontrado, aplica null se não
             })
         }
         return arrayProdutosLINX;
-    } catch(erro){
+    } catch (erro) {
         console.error(erro)
     }
 }
 
-// obterProdutosVTEX(produtosVTEXCSV).then((result) => {
-//     console.log(result)
-// }).catch((erro) =>{
-//     console.log(erro)
-// })
+// Array com as promises para usar no Promise.all e aguardar a resolucao das duas 
+const promises = [obterProdutosVTEX(produtosVTEXCSV), obterProdutosLINX(produtosLINXCSV)]
 
-obterProdutosLINX(produtosLINXCSV).then((result) => {
-    console.log(result)
-}).catch((erro) =>{
-    console.log(erro)
+//Promise.all para esperar que ambas as promises sejam resolvidas
+Promise.all(promises).then(([produtosVTEX, produtosLINX]) => {
+    compararArrays(produtosVTEX, produtosLINX)
+}).catch((erro) => {
+    console.error(erro)
 })
 
 
-console.log(produtosLINXCSV)
+// Compara dois array e re
+function compararArrays(arr1, arr2) {
+    let arrayUm = arr1;
+    let arrayDois = arr2;
+    // console.log(produtosVtex)
+    // console.log(produtosLinx) 
+    for (let i = 0; i < arrayUm.length; i++) {
+        for (let j = 0; j < arrayDois.length; j++) {
+            if (arrayUm[i].skuProduto == arrayDois[j].skuProduto) {
+                produtosIguais.push({
+                    Produto1: arrayUm[i],
+                    Produto2: arrayDois[j]
+                });
+            }
+        }
+    }
+
+    writeCsvFile(produtosIguais, caminhoArquivo)
+    return produtosIguais
+}
+
+
+// Transformar o objeto retornado em array 
+function writeCsvFile(dados, path) {
+    // Criando as colunas 
+    const csvWriter = createObjectCsvWriter({
+        path: path,
+        header: [
+            {id: 'Produto1_nome', title: 'Produto1: nome'},
+            {id: 'Produto1_link', title: 'Produto1: link'},
+            {id: 'Produto1_skuProduto', title: 'Produto1: sku produto'},
+            {id: 'Produto2_nome', title: 'Produto2: nome'},
+            {id: 'Produto2_link', title: 'Produto2: link'},
+            {id: 'Produto2_skuProduto',title: 'Produto2: sku produto'}
+        ]
+    })
+
+    // Transformando os dados do objeto no formato para csv
+    const dadosParaCSV = dados.map(item => ({
+        Produto1_nome: item.Produto1.nome,
+        Produto1_link: item.Produto1.link,
+        Produto1_skuProduto: item.Produto1.skuProduto,
+        Produto2_nome: item.Produto2.nome,
+        Produto2_link: item.Produto2.link,
+        Produto2_skuProduto: item.Produto2.skuProduto,
+    }));
+    
+
+    csvWriter.writeRecords(dadosParaCSV).then(() => {
+        console.log(`Dados foram gravados em ${caminhoArquivo}`);
+    }).catch((err) => {
+        console.error('Erro ao gravar no arquivo CSV:', err);
+    });
+}
